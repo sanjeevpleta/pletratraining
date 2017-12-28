9f302bd5b678644307319682348dd269470a71e5
sanjeevpleta <34168820+sanjeevpleta@users.noreply.github.com>
2017-12-27 20:25:39 -0500

    Add files via upload

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -8,12 +8,17 @@ import { ProfileProvider } from '../../providers/profile/profile';
 //import { workshop } from '../../interface/workshop';
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+    selector: 'page-home',
+    templateUrl: 'home.html'
 })
 export class HomePage {
 
     public workshopData: Array<any> = null;
+    public SalesforceAdmin: Array<any> = null;
+    public SalesforceDeveloper: Array<any> = null;
+    public Workshop: boolean = false;
+    public Admin: boolean = false;
+    public Developer: boolean = false;
     public filter: string;
     public isAdmin: boolean = false;
 
@@ -24,41 +29,49 @@ export class HomePage {
         var p: string = this.navParam.get('param');
         switch (p) {
             case 'SW':
-                this.filter = 'Salesforce Workshop';
+                this.Workshop = true;
                 break;
             case 'SA':
-                this.filter = 'Salesforce Admin';
+                this.Admin = true;
                 break;
             case 'SD':
-                this.filter = 'Salesforce Developer';
+                this.Developer = true;
                 break;
             default:
-                this.filter = '';
+                this.Workshop = true;
+                this.Admin = true;
+                this.Developer = true;
         }
 
 
-        
+
         //console.log('user = ' + user);
     }
 
     ionViewDidLoad() {
         this.provider.get().on('value', workshopListSnapshot => {
-            this.workshopData = [];
-            workshopListSnapshot.forEach(snap => {
-                if (snap.val().eventType == this.filter || this.filter == '') {
-                this.workshopData.push({
-                    id: snap.key,
-                    startDate: snap.val().startDate,
-                    cost: snap.val().cost,
-                    location: snap.val().location,
-                    duration: snap.val().duration
-                });
-            }
-                return false;
-            });
+            this.workshopData = this.Getdata(workshopListSnapshot, 'Salesforce Workshop');
+            this.SalesforceAdmin = this.Getdata(workshopListSnapshot, 'Salesforce Admin');
+            this.SalesforceDeveloper = this.Getdata(workshopListSnapshot, 'Salesforce Developer');
         });
 
         this.isAdmin = this.getAdmin();
+    }
+
+    Getdata(data: any, filter: string): Array<any> {
+        var array: Array<any> = [];
+        data.forEach(snap => {
+            if (snap.val().eventType == filter) {
+                array.push({
+                    id: snap.key,
+                    startDate: snap.val().startDate,
+                    cost: snap.val().cost,
+                    location: snap.val().location
+                });
+            }
+            return false;
+        });
+        return array;
     }
 
     getAdmin(): boolean {
@@ -70,14 +83,14 @@ export class HomePage {
             }
 
         });
-        return retVal;  
+        return retVal;
     }
 
-  addTraining(): void {
-      this.navCtrl.push(AddTrainingPage);
+    addTraining(): void {
+        this.navCtrl.push(AddTrainingPage);
     }
 
-  goToDetail(id): void {
-      this.navCtrl.push('TrainingDetailPage', {id: id});
-  }
+    goToDetail(id): void {
+        this.navCtrl.push('TrainingDetailPage', { id: id });
+    }
 }
