cfd42804e38c469ad4a20d687c2a1385f1ad5fdc
sanjeevpleta <34168820+sanjeevpleta@users.noreply.github.com>
2018-01-01 21:50:46 -0500

    Add files via upload

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -13,8 +13,8 @@ import { WorkshopProvider } from '../../providers/workshop/workshop';
 
 @IonicPage()
 @Component({
-  selector: 'page-profile',
-  templateUrl: 'profile.html',
+    selector: 'page-profile',
+    templateUrl: 'profile.html',
 })
 export class ProfilePage {
     public userProfile: any;
@@ -27,146 +27,155 @@ export class ProfilePage {
         public profileProvider: ProfileProvider,
         public eventProvider: WorkshopProvider
     ) {
-  }
+    }
 
-  ionViewDidLoad() {
-      this.profileProvider.getUserProfile().on('value', userProfileSnapshot => {
-          this.userProfile = userProfileSnapshot.val();
-          this.birthDate = userProfileSnapshot.val().birthDate;
-          this.mobileNumber = userProfileSnapshot.val().mobileNumber;
-          //userProfileSnapshot.val().registeredEvents.forEach(snap => {
-          //    this.registeredEvents.push({
-          //        status: snap.val().status
-          //    });
-          //});
-      });
+    ionViewDidLoad() {
+        if (this.profileProvider.isUserVerified() == false) {
+            const alert: Alert = this.alertCtrl.create({
+                message: "Email address is not verified.",
+                buttons: [{ text: 'Ok', role: 'cancel' }]
+            });
+            alert.present();
+            this.navCtrl.setRoot('LoginPage');
+        }
 
-      this.profileProvider.getRegisteredEvents().on('value', snap => {
-          this.registeredEvents = [];
-          snap.forEach(data => {
-              let value: any = this.getEvent(data.val().eventId);
-              console.log('value = ' + value);
-              this.registeredEvents.push({
-                  eventType: value.eventType,
-                  date: value.startDate,
-                  location: value.location,
-                  status: data.val().status,
-                  eventId: data.key
-              });
-              return false;
-          });
-      });
+        this.profileProvider.getUserProfile().on('value', userProfileSnapshot => {
+            this.userProfile = userProfileSnapshot.val();
+            this.birthDate = userProfileSnapshot.val().birthDate;
+            this.mobileNumber = userProfileSnapshot.val().mobileNumber;
+            //userProfileSnapshot.val().registeredEvents.forEach(snap => {
+            //    this.registeredEvents.push({
+            //        status: snap.val().status
+            //    });
+            //});
+        });
 
-      //console.log('Profile = ' + this.userProfile.registeredEvents);
-  }
-  getEvent(eventID: string): any {
-      let value: any = '';
-      //console.log('event id = ' + eventID);
-      this.eventProvider
-          .getById(eventID)
-          .on('value', eventData => {
-              console.log('eventdata = ' + eventData.val().eventType);
-              value = eventData.val()
-          });
-      return value;
-  }
+        this.profileProvider.getRegisteredEvents().on('value', snap => {
+            this.registeredEvents = [];
+            snap.forEach(data => {
+                let value: any = this.getEvent(data.val().eventId);
+                console.log('value = ' + value);
+                this.registeredEvents.push({
+                    eventType: value.eventType,
+                    date: value.startDate,
+                    location: value.location,
+                    status: data.val().status,
+                    eventId: data.key
+                });
+                return false;
+            });
+        });
 
-  unRegister(key: string): void {
-      //console.log("key = " + key);
-      this.profileProvider.unRegister(key);
-  }
+        //console.log('Profile = ' + this.userProfile.registeredEvents);
+    }
+    getEvent(eventID: string): any {
+        let value: any = '';
+        //console.log('event id = ' + eventID);
+        this.eventProvider
+            .getById(eventID)
+            .on('value', eventData => {
+                console.log('eventdata = ' + eventData.val().eventType);
+                value = eventData.val()
+            });
+        return value;
+    }
 
-  logOut(): void {
-      this.authProvider.logoutUser().then(() => {
-          this.navCtrl.setRoot('LoginPage');
-      });
-  }
+    unRegister(key: string): void {
+        //console.log("key = " + key);
+        this.profileProvider.unRegister(key);
+    }
 
-  updateName(): void {
-      const alert: Alert = this.alertCtrl.create({
-          message: 'Your first name & last name',
-          inputs: [
-              {
-                  name: 'firstName',
-                  placeholder: 'Your first name',
-                  value: this.userProfile.firstName
-              },
-              {
-                  name: 'lastName',
-                  placeholder: 'Your last name',
-                  value: this.userProfile.lastName
-              }
-          ],
-          buttons: [
-              { text: 'Cancel' },
-              {
-                  text: 'Save',
-                  handler: data => {
-                      this.profileProvider.updateName(data.firstName, data.lastName);
-                  }
-              }
-          ]
-      });
-      alert.present();
-  }
+    logOut(): void {
+        this.authProvider.logoutUser().then(() => {
+            this.navCtrl.setRoot('LoginPage');
+        });
+    }
 
-  updateDOB(birthDate: string): void {
-      this.profileProvider.updateDOB(birthDate);
-  }
+    updateName(): void {
+        const alert: Alert = this.alertCtrl.create({
+            message: 'Your first name & last name',
+            inputs: [
+                {
+                    name: 'firstName',
+                    placeholder: 'Your first name',
+                    value: this.userProfile.firstName
+                },
+                {
+                    name: 'lastName',
+                    placeholder: 'Your last name',
+                    value: this.userProfile.lastName
+                }
+            ],
+            buttons: [
+                { text: 'Cancel' },
+                {
+                    text: 'Save',
+                    handler: data => {
+                        this.profileProvider.updateName(data.firstName, data.lastName);
+                    }
+                }
+            ]
+        });
+        alert.present();
+    }
 
-  updateEmail(): void {
-      let alert: Alert = this.alertCtrl.create({
-          inputs: [
-              { name: 'newEmail', placeholder: 'Your new email' },
-              { name: 'password', placeholder: 'Your password', type: 'password' }
-          ],
-          buttons: [
-              { text: 'Cancel' },
-              {
-                  text: 'Save',
-                  handler: data => {
-                      this.profileProvider
-                          .updateEmail(data.newEmail, data.password)
-                          .then(() => {
-                              console.log('Email Changed Successfully');
-                          })
-                          .catch(error => {
-                              console.log('ERROR: ' + error.message);
-                          });
-                  }
-              }
-          ]
-      });
-      alert.present();
-  }
+    updateDOB(birthDate: string): void {
+        this.profileProvider.updateDOB(birthDate);
+    }
 
-  updatePassword(): void {
-      let alert: Alert = this.alertCtrl.create({
-          inputs: [
-              { name: 'newPassword', placeholder: 'New password', type: 'password' },
-              { name: 'oldPassword', placeholder: 'Old password', type: 'password' }
-          ],
-          buttons: [
-              { text: 'Cancel' },
-              {
-                  text: 'Save',
-                  handler: data => {
-                      this.profileProvider.updatePassword(
-                          data.newPassword,
-                          data.oldPassword
-                      );
-                  }
-              }
-          ]
-      });
-      alert.present();
-  }
+    updateEmail(): void {
+        let alert: Alert = this.alertCtrl.create({
+            inputs: [
+                { name: 'newEmail', placeholder: 'Your new email' },
+                { name: 'password', placeholder: 'Your password', type: 'password' }
+            ],
+            buttons: [
+                { text: 'Cancel' },
+                {
+                    text: 'Save',
+                    handler: data => {
+                        this.profileProvider
+                            .updateEmail(data.newEmail, data.password)
+                            .then(() => {
+                                console.log('Email Changed Successfully');
+                            })
+                            .catch(error => {
+                                console.log('ERROR: ' + error.message);
+                            });
+                    }
+                }
+            ]
+        });
+        alert.present();
+    }
 
-  updateMobile(mobileNumber: number): void {
-      this.profileProvider.updateMobile(mobileNumber);
-  }
+    updatePassword(): void {
+        let alert: Alert = this.alertCtrl.create({
+            inputs: [
+                { name: 'newPassword', placeholder: 'New password', type: 'password' },
+                { name: 'oldPassword', placeholder: 'Old password', type: 'password' }
+            ],
+            buttons: [
+                { text: 'Cancel' },
+                {
+                    text: 'Save',
+                    handler: data => {
+                        this.profileProvider.updatePassword(
+                            data.newPassword,
+                            data.oldPassword
+                        );
+                    }
+                }
+            ]
+        });
+        alert.present();
+    }
 
-  goToHome(): void {
-      this.navCtrl.setRoot(HomePage);
-  }
+    updateMobile(mobileNumber: number): void {
+        this.profileProvider.updateMobile(mobileNumber);
+    }
+
+    goToHome(): void {
+        this.navCtrl.setRoot(HomePage);
+    }
 }
