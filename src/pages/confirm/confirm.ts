a261a12c662995d966725300c8553588e43cae08
sanjeevpleta <34168820+sanjeevpleta@users.noreply.github.com>
2018-01-07 08:48:40 -0500

    Add files via upload

--- src/pages/confirm/confirm.ts
+++ src/pages/confirm/confirm.ts
@@ -1,29 +1,51 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { HomePage } from '../home/home';
-//import { EmailComposer } from '@ionic-native/email-composer';
-
+import { EmailProvider } from '../../providers/email/email';
 @IonicPage()
 @Component({
   selector: 'page-confirm',
   templateUrl: 'confirm.html',
 })
 export class ConfirmPage {
-
+    public currentEvent: any = {};
+    public userProfile: any = {};
+    private message: string = "";
     constructor(public navCtrl: NavController, public navParams: NavParams
-        //, private emailComposer: EmailComposer
+        , private emailProvider: EmailProvider
     ) {
   }
 
-  ionViewDidLoad() {
-      let email = {
-          to: 'max@mustermann.de',
-          cc: 'info@pletratech.com',
-          subject: 'Pletra training - Payment details',
-          body: 'Dear student \n Thanks for registering Salesforce workshop. \n Here are our bank details to transfer fee for confirmation. \n Bank Name: State bank of India \n Account: 0023452345234 \n IFSC: SBIN000234',
-          isHtml: true
-      };
-  }
+    ionViewDidLoad() {
+        this.currentEvent = this.navParams.get('eventData');
+        this.userProfile = this.navParams.get('profile');
+
+        this.getMessage();
+
+        this.emailProvider.sendEmail(this.userProfile.email,
+            '',
+            'info@pletratech.com',
+            '',
+            'Pletra training - Payment details',
+            'Dear student \n Thanks for registering ' + this.currentEvent.eventType + '.\n Here are our bank details to transfer fee for confirmation.\n' + this.message
+        );
+    }
+
+    getMessage() {
+        
+        this.emailProvider.getMessage().on('value', messagesnap => {
+            messagesnap.forEach(snap => {
+                this.message = snap.val().message
+                //console.log('message = ' + this.message);
+                return false;
+            });
+
+            if (this.message == null) {
+                this.message = '2. Bank name: State bank of India \nBank account name: Pletratech Pvt ltd. \nAccount number: 000023452345 \nIFSC: SBIN0002345';                
+            }
+
+        });
+    }
 
   goBackHome(): void {
       this.navCtrl.setRoot(HomePage);
