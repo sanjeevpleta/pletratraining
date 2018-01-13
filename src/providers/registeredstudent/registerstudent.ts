037dc5e768652dc720228e892cc2db893d984922
sanjeevpleta <34168820+sanjeevpleta@users.noreply.github.com>
2018-01-13 08:34:40 -0500

    Add files via upload

---
+++ src/providers/registeredstudent/registerstudent.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import firebase from 'firebase';
+
+@Injectable()
+export class RegisterstudentProvider {
+    public registeredEvent: firebase.database.Reference;
+    public currentUser: firebase.User;
+    constructor() {
+         firebase.auth().onAuthStateChanged(user => {
+             if (user) {
+                 this.currentUser = user;
+                 this.registeredEvent = firebase.database().ref('registerevent');
+                 console.log('Registered Student list : ' + this.registeredEvent);
+               }
+          });
+    }
+
+  createRegisterStudent(eventStatus: string, userId: string, eventId: string): firebase.database.ThenableReference {
+      console.log('Registered Student list : ' + this.registeredEvent);
+      return this.registeredEvent.push({
+          status: eventStatus,
+          userdata: this.currentUser.uid,
+          eventdata: eventId,
+      });
+
+  }
+
+}
