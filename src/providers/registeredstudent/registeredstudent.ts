b9cf2d6139e75d2957c848f5c58a5774d4a39b5e
surajpletratech <34180713+surajpletratech@users.noreply.github.com>
2018-01-15 19:16:13 +0530

    Add files via upload

--- src/providers/registeredstudent/registeredstudent.ts
+++ src/providers/registeredstudent/registeredstudent.ts
@@ -1,32 +1,35 @@
+import { Injectable } from '@angular/core';
 import firebase from 'firebase';
 
-import { Injectable } from '@angular/core';
 @Injectable()
-
-
 export class RegisterStudentProvider {
-	public registeredEvent: firebase.database.Reference;
-    
-	constructor() {
-	       // firebase.auth().onAuthStateChanged(user => {
-           //  if (user) {
-                this.registeredEvent = firebase.database().ref('registerEvent');
-                console.log('Registered Student list : ' + this.registeredEvent);
-           //  }
-        //});
-        }
+    public registeredEvent: firebase.database.Reference;
+    public currentUser: firebase.User;
+    constructor() {
+         firebase.auth().onAuthStateChanged(user => {
+             if (user) {
+                 this.currentUser = user;
+                 this.registeredEvent = firebase.database().ref('registerevent');
+                 console.log('Registered Student list : ' + this.registeredEvent);
+				 //console.log('status:'+eventStatus);
+               }
+          });
+    }
 
-    createRegisterStudent(eventStatus:string,userId:string,eventId:string):firebase.database.ThenableReference{
-       return this.registeredEvent.push({
-	        status:eventStatus,
-			user:userId,
-			event:eventId,
-	                
-	   });
-	}
-	getRegisteredStudent(): firebase.database.Reference {
-       return this.registeredEvent;
-     }
-  
- } 
-  
+  createRegisterStudent(eventStatus: string, userId: string, eventId: string): firebase.database.ThenableReference {
+      console.log('Registered Student lit : ' + this.registeredEvent);
+	  console.log('Registered status : ' + eventStatus);
+	  console.log('user:'+this.currentUser.uid);
+	      return this.registeredEvent.push({
+          status: eventStatus,
+          userdata: this.currentUser.uid,
+          eventdata: eventId,	  
+      });
+	  
+   } 
+   getRegisteredStudent(): firebase.database.Reference {
+      return this.registeredEvent;
+	  
+  }
+    
+}
